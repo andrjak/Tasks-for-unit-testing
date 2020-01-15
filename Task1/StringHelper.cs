93894b0a4ab118a92611fbe8520c59a2452a6a96
andrjak <79821843rt@gmail.com>
2020-01-15 23:57:37 +0300

    feat: Method GetGcd and GetVowelCount implementation add

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,12 +2,49 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// Class that helps to work with string.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Method for counting vowels in a string.
+        /// </summary>
+        /// <param name="str">Vowel search string.</param>
+        /// <returns>Number of vowels per line.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// If the line == null.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// If the line is empty.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// int count = GetVowelCount("example");
+        /// </code>
+        /// </example>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str), "String cannot be null.");
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,13 +1,60 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// A class extending the capabilities of integer types.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// A method that helps find GCD.
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second numder.</param>
+        /// <returns>Greatest common divisor.</returns>
+        /// <exception cref="System.ArgumentException">
+        /// If both numbers are zero.
+        /// </exception>
+        /// <exception cref="System.OverflowException">
+        /// In case of overflow.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// int result = GetGcd(30, 12);
+        /// </code>
+        /// </example>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a == 0)
+            {
+                return Math.Abs(b);
+            }
+
+            if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+
+            checked
+            {
+                while (true)
+                {
+                    if ((a %= b) == 0)
+                    {
+                        return Math.Abs(b);
+                    }
+                    else if ((b %= a) == 0)
+                    {
+                        return Math.Abs(a);
+                    }
+                }
+            }
         }
     }
 }
